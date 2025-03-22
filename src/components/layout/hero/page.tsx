8ae75c4876787efd39cd4a6558f37f82b0fe4747
João Victor Sant'Ana Cortabitart <joaovictorcortabitart@gmail.com>
2025-03-22 10:23:06 -0300

    hero

--- src/components/layout/hero/page.tsx
+++ src/components/layout/hero/page.tsx
@@ -5,15 +5,6 @@ export const Title = () => {
   return (
     <div className="w-full">
       <div className="bg-background dark:bg-background-dark text-text dark:text-text-dark flex flex-col items-center justify-center min-h-[80vh] px-4">
-        {/*
-        <Image
-          src="https://cdn-icons-png.freepik.com/512/18/18148.png"
-          width={50}
-          height={50}
-          alt="Picture of the author"
-          className="mt-8 mb-4 md:mt-16 md:mb-8"
-        />*/}
-
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center max-w-[90%]">
           Software Engineer
         </h1>
@@ -27,3 +18,13 @@ export const Title = () => {
     </div>
   );
 };
+{
+  /*     {/*
+        <Image
+          src="https://cdn-icons-png.freepik.com/512/18/18148.png"
+          width={50}
+          height={50}
+          alt="Picture of the author"
+          className="mt-8 mb-4 md:mt-16 md:mb-8"
+        />*/
+}
